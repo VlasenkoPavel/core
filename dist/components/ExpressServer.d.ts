06f5fbc2cc47ee57d183f947aa292138df9febac
VlasenkoPavel <psvlasenko@gmail.com>
2019-05-20 19:05:10 +0300

    in progress

--- dist/components/ExpressServer.d.ts
+++ dist/components/ExpressServer.d.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import { Logger } from 'log4js';
 import { Server } from 'http';
 import { ServerConfig } from '@chaika/config';
-import { IContainer } from 'types';
+import { IContainer } from '../types';
 export declare class ExpressServer {
     readonly express: express.Application;
     private logger;

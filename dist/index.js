73443a00a532e173325aeb0ff9c948dfc91b08b4
VlasenkoPavel <psvlasenko@gmail.com>
2019-05-20 17:49:20 +0300

    in progress

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,7 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./Application"));
 __export(require("./ConsoleApp"));
+__export(require("./components/ExpressServer"));
+__export(require("./components/middlewares/ErrorHandlingMiddleware"));
+__export(require("./components/DbConnector"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './Application';
 export * from './ConsoleApp';
+export * from './components/ExpressServer';
+export * from './components/middlewares/ErrorHandlingMiddleware';
+export * from './components/DbConnector';
 export * from './types';

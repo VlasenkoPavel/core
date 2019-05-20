27b5757ddec2c2fbb1fb309eb39712d6bed2b0f0
VlasenkoPavel <psvlasenko@gmail.com>
2019-05-20 18:09:19 +0300

    in progress

--- src/components/LoggerFactory.ts
+++ src/components/LoggerFactory.ts
@@ -1,5 +1,5 @@
 import * as log4js from 'log4js';
-import { LogConfig, LogCategoryConfig } from '@c7s/config';
+import { LogConfig, LogCategoryConfig } from '@chaika/config';
 
 export class LoggerFactory {
     protected readonly logConfig!: LogConfig;
